1123d03e42ea166fd1c47883df06376bd456f234
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-25 05:01:58 +0700

    PHP Web Hello World, PHP Web

---
+++ 5-php-web-hello-world/hello-world.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello World.</br>";
+
+echo "<h1>Hello World</h1>" . PHP_EOL;
+echo "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sit ipsum, odio quasi velit hic illum suscipit nemo quia beatae ab? Voluptatem nulla omnis quaerat est minima nihil voluptas temporibus?</p> </br>" . PHP_EOL;
+
+echo "<h1>Hello World</h1>" . PHP_EOL;
+echo "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti quod corporis aspernatur, repellendus earum, tenetur necessitatibus ipsam debitis veniam nisi explicabo iusto dolorum voluptatibus. Laborum quae exercitationem ipsa ratione?</p> </br>" . PHP_EOL;
+
+echo "<h1>Hello World</h1>" . PHP_EOL;
+echo "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti quod corporis aspernatur, repellendus earum, tenetur necessitatibus ipsam debitis veniam nisi explicabo iusto dolorum voluptatibus. Laborum quae exercitationem ipsa ratione?</p> </br>" . PHP_EOL;
+
