7af90cc7d0a6a19d71ebbf182fcd3f1757956315
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-27 10:05:49 +0700

    Cookie, PHP Web

---
+++ 16-cookie/cookie.php
@@ -0,0 +1,5 @@
+<?php
+
+setcookie('X-BELAJAR-COOKIE', 'Terry Andrew Davis'); # function untuk membuat cookie. parameter pertama adalah key, parameter kedua adalah value.
+
+header('Location: /16-cookie/show-cookie.php'); # function untuk redirect ke halaman website tertentu.
---
+++ 16-cookie/show-cookie.php
@@ -0,0 +1,9 @@
+<?php
+
+$belajarCookie = $_COOKIE['X-BELAJAR-COOKIE']; # Mengambil Cookie yang dikirim oleh client dengan global variable $_COOKIE.
+
+echo "<h1>$belajarCookie</h1>";
+echo "<p>" .
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo labore aperiam dolor debitis nisi facere corrupti porro officiis in mollitia dicta reprehenderit voluptas distinctio eos, tenetur laudantium corporis sint odio?
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste dolor sapiente provident nulla ab magni tempora nam at explicabo animi illum, commodi esse architecto dolores labore ullam sint nostrum officiis?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cumque consequuntur. Facere qui veritatis reiciendis commodi asperiores, pariatur nam eum tempore non corrupti quos quia eius officia voluptate doloribus dolore." . "</p>";
